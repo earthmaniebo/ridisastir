e9b7c32092cf250d44148619dbd1c145005c69a3
Earth Maniebo <earthmaniebo@gmail.com>
2014-05-10 23:43:51 +0800

    Add explicit table def in City model

--- app/models/City.php
+++ app/models/City.php
@@ -1,5 +1,8 @@
 <?php
 
 class City extends \Eloquent {
+
+    // Need to explicitly specify because laravel adds 's' to pluralize models
+    protected $table = 'cities';
 	protected $fillable = [];
 }

f86c7a9bbb4873cb978ce91f9b8ec64e3749618b
Earth Maniebo <earthmaniebo@gmail.com>
2014-05-11 02:12:06 +0800

    Fix duplicate

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -21,30 +21,7 @@
                     <span class="fa fa-location-arrow fa-fw"></span> 
                 </button>
             </div>
-<div class="medium-12 columns" id="loginRegister">
-    <a href="#" data-reveal-id="loginModal">Login</a>
-</div>
-<div class="row pagepanel" id="masthead">
-    <div id="mastbody">
-        <div class="small-6 small-offset-3 medium-5 medium-offset-0 columns" id="maplogo" >
-            <img src="assets/img/ph_new1.png">
-        </div>
-        <div class="small-11 small-offset-1 medium-7 medium-offset-0 columns" id="mastcontent">
-            <h1>Ridisastir</h1>
-            <h3><i>Asset Management System</i></h3>
-            <br>
-            <button class="button small-11 medium-4 medium-offset-0" id="requestButton" data-reveal-id="requestModal">
-            <span class="fa fa-flag fa-fw"></span> 
-            Request
-            </button>
-            <button class="button small-11 medium-4 medium-offset-0" id="browseButton">
-            Browse
-            <span class="fa fa-location-arrow fa-fw"></span> 
-            </button>
-        </div>
-    </div>
-</div>
-<div id="requestModal" class="reveal-modal" data-reveal>
+    <div id="requestModal" class="reveal-modal" data-reveal>
         <form>
             <div class="row">
                 <div class="medium-12 columns">

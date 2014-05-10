462c22d6afd39e98932421fb9b10e6f63179845b
Earth Maniebo <earthmaniebo@gmail.com>
2014-05-11 01:42:57 +0800

    Added login form, fix indentation, added names and id to inputs

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="medium-12 columns" id="loginRegister">
-        <a href="#" data-reveal-id="loginModal">Login</a> | <a href="#" data-reveal-id="registerModal">Register</a>
+        <a href="#" data-reveal-id="loginModal">Login</a>
     </div>
     <div class="row pagepanel" id="masthead">
         <div id="mastbody">
@@ -25,49 +25,65 @@
         </div>
     </div>
     <div id="requestModal" class="reveal-modal" data-reveal>
-        
         <form>
-          <div class="row">
-            <div class="medium-12 columns">
-              <label>Name
-                <input type="text" placeholder="..." />
-              </label>
-            </div>
-            <div class="medium-12 columns">
-              <label>Contact Number
-                <input type="text" placeholder="..." />
-              </label>
-            </div>
-            <div class="medium-12 columns">
-              <label>Request
-                <textarea rows="4" paceholder="medium-4.columns" ></textarea>
-              </label>
-            </div>
-            <div class="medium-12 columns">
-              <label>Barangay
-                <select>
-                  <option value="husker">Husker</option>
-                  <option value="starbuck">Starbuck</option>
-                  <option value="hotdog">Hot Dog</option>
-                  <option value="apollo">Apollo</option>
-                </select>
-              </label>
-            </div>
             <div class="row">
-                <button class="button small-4 small-offset-4">Submit</button>
-            </div>
+                <div class="medium-12 columns">
+                    <label>Name
+                        <input type="text" name="name" id="name" placeholder="..." />
+                    </label>
+                </div>
+                <div class="medium-12 columns">
+                    <label>Contact Number
+                        <input type="text" name="contact_no" id="contact_no" placeholder="..." />
+                    </label>
+                </div>
+                <div class="medium-12 columns">
+                    <label>Request
+                        <textarea rows="4" name="request" id="request" paceholder="medium-4.columns" ></textarea>
+                    </label>
+                </div>
+                <div class="medium-12 columns">
+                    <label>Barangay
+                        <select name="user_id">
+                            <option value="1">Husker</option>
+                            <option value="2">Starbuck</option>
+                            <option value="3">Hot Dog</option>
+                            <option value="4">Apollo</option>
+                        </select>
+                    </label>
+                </div>
+                <div class="row">
+                    <input type="submit" name="request-btn" value="Request" class="button small-4 small-offset-4" />
+                </div>
           </div>
       </form>
         <a class="close-reveal-modal">&#215;</a>
     </div>
-    <div id="loginModal" class="reveal-modal" data-reveal>
+    <div id="loginModal" class="reveal-modal medium" data-reveal>
         <h2>Login</h2>
-        <p class="lead">Your couch. It is mine.</p>
-        <p>Im a cool paragraph that lives inside of an even cooler modal. Wins</p>
+        <form>
+            <div class="row">
+                 <div class="medium-6 medium-offset-3 columns">   
+                    <div class="medium-12 columns">
+                        <label>Email
+                            <input type="email" name="email" id="email" placeholder="..." />
+                        </label>
+                    </div>
+                    <div class="medium-12 columns">
+                        <label>Password
+                            <input type="password" name="password" id="password" placeholder="..." />
+                        </label>
+                    </div>            
+                    <div class="row">
+                        <input type="submit" name="login-btn" value="Login" class="button small-4 small-offset-4" />
+                    </div>
+                </div>
+          </div>
+      </form>
         <a class="close-reveal-modal">&#215;</a>
     </div>
     <div id="registerModal" class="reveal-modal" data-reveal>
-        <h2>Register</h2>
+        <h2>Register your Department / Barangay</h2>
         <p class="lead">Your couch. It is mine.</p>
         <p>Im a cool paragraph that lives inside of an even cooler modal. Wins</p>
         <a class="close-reveal-modal">&#215;</a>

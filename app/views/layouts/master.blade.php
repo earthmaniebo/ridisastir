941a3dd65a1f1dbc5ff604f20b3356125dc7a963
Rwll <rowell.dcanlapan@gmail.com>
2014-05-11 04:33:59 +0800

    pulse effect on map img

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -5,11 +5,13 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Welcome to Ridisastir | Home</title>
+<link rel="stylesheet" href="assets/css/animation.css">
 <link rel="stylesheet" href="assets/css/normalize.css">
 <link rel="stylesheet" href="assets/css/foundation.css">
 <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 <link rel="stylesheet" href="assets/css/home.css">
 <script src="assets/js/vendor/modernizr.js"></script>
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
 <body>
     @yield('content')
@@ -17,8 +19,27 @@
 <script src="assets/js/vendor/jquery.js"></script>
 <script src="assets/js/foundation.min.js"></script>
 <script src="assets/js/home.js"></script>
+
 <script>
 $(document).foundation();
+</script>
+<script>
+	$(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("pulse");
+			}
+		});
+	});
+</script>
+
+<script>
+	$('#animatedElement').click(function() {
+		$(this).addClass("pulse");
+	});
 </script>
 </body>
 </html>

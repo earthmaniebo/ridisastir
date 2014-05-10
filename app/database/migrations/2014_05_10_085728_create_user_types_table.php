e13bd98dabeca8728843de7353b636af1f59eb18
Earth Maniebo <earthmaniebo@gmail.com>
2014-05-10 17:22:59 +0800

    Create user types table

---
+++ app/database/migrations/2014_05_10_085728_create_user_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTypesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        // Create the table.
+        Schema::create('user_types', function($table) {
+            $table->increments('id');
+            $table->string('user_type', 128);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop table
+		Schema::drop('user_types');
+	}
+
+}

c24e891740cf21e722e00095deced8cc98f88b86
Earth Maniebo <earthmaniebo@gmail.com>
2014-05-10 17:22:03 +0800

    Configure database conn

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'ridisastir',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'earth',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
